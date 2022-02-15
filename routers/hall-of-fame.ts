1f36bfb5ef7d61015eb0bb01f3c8619042ccf847
xmNuc <xmnuc@o2.pl>
2022-02-15 02:02:04 +0100

    1.72 add hall of  fames working code

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import { WarriorRecord } from '../records/warrior.record';
 
 export const hallOfFameRouter = Router();
 
-hallOfFameRouter.get('/', (req, res) => {
-  res.render('hall-of-fame/list');
+hallOfFameRouter.get('/', async (req, res) => {
+  const warriors = (await WarriorRecord.listTop(10)).map((warrior, index) => {
+    return {
+      place: index + 1,
+      warrior,
+    };
+  });
+  res.render('hall-of-fame/list', { warriors });
 });
