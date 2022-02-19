a314965a71c830f5d8c69d6bafb78eb3384ed0cb
xmNuc <xmnuc@o2.pl>
2022-02-19 03:48:05 +0100

    1.9 commit

--- dist/utils/errors.js
+++ dist/utils/errors.js
@@ -1,9 +1,9 @@
-export class ValidationError extends Error {
-}
+export class ValidationError extends Error {}
 export const handleError = (err, req, res, next) => {
-    console.error(err);
-    res.status(err instanceof ValidationError ? 400 : 500).render('error', {
-        message: err instanceof ValidationError ? err.message : 'Sorry try again later',
-    });
+  console.error(err);
+  res.status(err instanceof ValidationError ? 400 : 500).render('error', {
+    message:
+      err instanceof ValidationError ? err.message : 'Sorry try again later',
+  });
 };
 //# sourceMappingURL=errors.js.map
